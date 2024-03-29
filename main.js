e48cecddf096ed3c54f6ff7300734043f36eb40f
Aqsa_Saeed <163544416+Akksaa@users.noreply.github.com>
2024-03-30 03:48:22 +0500

    files added

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let toDos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in your Todos?!"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Are you sure you want to add more tasks?!",
+            default: "false"
+        }
+    ]);
+    toDos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(toDos);
+}
+;
